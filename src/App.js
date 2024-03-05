0d36d2e0eca22b153710c8cf0faeb9ccd99d661a
Heedrhiss <muyideenidris77@gmail.com>
2024-03-05 13:58:08 +0100

    updated inital array

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import "./index.css";
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Heedrhiss",
     image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
+    id: 933372,
+    name: "Orochimaru",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: -7,
+  },
+  {
     id: 499476,
-    name: "Anthony",
+    name: "Black-Beard",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
